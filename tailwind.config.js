6f2e08ade7db704316a746082276527e7178c5f1
Prakash Kumar <141754712+prakash-ydv@users.noreply.github.com>
2024-07-13 23:16:05 +0530

    Social Links Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,22 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      keyframes: {
+        'appear-from-bottom': {
+          '0%': { opacity: '0', transform: 'translateY(10px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+
+        'fade-in': {
+          '0%': { opacity: '0', transform: 'scale(0.65)' },
+          '100%': { opacity: '1', transform: 'scale(1)' },
+        },
+      },
+      animation: {
+        'fade-in': 'fade-in 0.3s ease-out forwards',
+        'appear-from-bottom': 'appear-from-bottom 0.2s ease-out forwards',
+      },
+
       fontFamily:{
         'poppins':['poppins'],
         'redit':['Reddit+Mono']
